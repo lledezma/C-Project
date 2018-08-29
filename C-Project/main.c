856d80e759e2c6ba731f66b4d91fabcf58e3f695
Luis <luis.ledezma@pop.belmont.edu>
2018-08-29 12:08:31 -0500

    second commit

--- C-Project/main.c
+++ C-Project/main.c
@@ -8,8 +8,32 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(int argc, const char * argv[]){
+
+    int n, num1, num2,result;
+    printf("What operation you wish to do? \n");
+    printf("#1 = sum\n");
+    printf("#2 = subtraction\n");
+    printf("#3 = Multiplication\n");
+    scanf("%d", &n);
+
+    printf("enter your numbers:");
+    scanf(" %d",&num1);
+    scanf("%d",&num2);
+
+    switch(n)
+    {
+        case 1: result= num1 + num2;
+            printf("%d\n", result);
+            break;
+        case 2: result = num1 - num2;
+            printf("%d\n", result);
+            break;
+        case 3: result = num1*num2;
+            printf("%d\n",result);
+            break;
+    }
+
+
     return 0;
 }
